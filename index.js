cb06a6d20403b7fdeb67b37c0d3e9a0e5d1448c6
Nick Camillo <nick.camillo@gmail.com>
2015-02-09 15:31:59 -0500

    remove doublecheck for now. make wrong answers still output correct play areao

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var words = ['happy'];
-var swords = ['acres', 'adult', 'advice', 'arrangement', 'attempt', 'August', 'Autumn', 'border', 'breeze', 'brick', 'calm', 'canal', 'Casey', 'cast', 'chose', 'claws', 'coach', 'constantly', 'contrast', 'cookies', 'customs', 'damage', 'Danny', 'deeply', 'depth', 'discussion', 'doll', 'donkey', 'Egypt', 'Ellen', 'essential', 'exchange', 'exist', 'explanation', 'facing', 'film', 'finest', 'fireplace', 'floating', 'folks', 'fort', 'garage', 'grabbed', 'grandmother', 'habit', 'happily', 'Harry', 'heading', 'hunter', 'Illinois', 'image', 'independent', 'instant', 'January', 'kids', 'label', 'Lee', 'lungs', 'manufacturing', 'Martin', 'mathematics', 'melted', 'memory', 'mill', 'mission', 'monkey', 'Mount', 'mysterious', 'neighborhood', 'Norway', 'nuts', 'occasionally', 'official', 'ourselves', 'palace', 'Pennsylvania', 'Philadelphia', 'plates', 'poetry', 'policeman', 'positive', 'possibly', 'practical', 'pride', 'promised', 'recall', 'relationship', 'remarkable', 'require', 'rhyme', 'rocky', 'rubbed', 'rush', 'sale', 'satellites', 'satisfied', 'scared', 'selection', 'shake', 'shaking', 'shallow', 'shout', 'silly', 'simplest', 'slight', 'slip', 'slope', 'soap', 'solar', 'species', 'spin', 'stiff', 'swung', 'tales', 'thumb', 'tobacco', 'toy', 'trap', 'treated', 'tune', 'University', 'vapor', 'vessels', 'wealth', 'wolf', 'zoo']; 
+var swords = ['happy'];
+var words = ['acres', 'adult', 'advice', 'arrangement', 'attempt', 'August', 'Autumn', 'border', 'breeze', 'brick', 'calm', 'canal', 'Casey', 'cast', 'chose', 'claws', 'coach', 'constantly', 'contrast', 'cookies', 'customs', 'damage', 'Danny', 'deeply', 'depth', 'discussion', 'doll', 'donkey', 'Egypt', 'Ellen', 'essential', 'exchange', 'exist', 'explanation', 'facing', 'film', 'finest', 'fireplace', 'floating', 'folks', 'fort', 'garage', 'grabbed', 'grandmother', 'habit', 'happily', 'Harry', 'heading', 'hunter', 'Illinois', 'image', 'independent', 'instant', 'January', 'kids', 'label', 'Lee', 'lungs', 'manufacturing', 'Martin', 'mathematics', 'melted', 'memory', 'mill', 'mission', 'monkey', 'Mount', 'mysterious', 'neighborhood', 'Norway', 'nuts', 'occasionally', 'official', 'ourselves', 'palace', 'Pennsylvania', 'Philadelphia', 'plates', 'poetry', 'policeman', 'positive', 'possibly', 'practical', 'pride', 'promised', 'recall', 'relationship', 'remarkable', 'require', 'rhyme', 'rocky', 'rubbed', 'rush', 'sale', 'satellites', 'satisfied', 'scared', 'selection', 'shake', 'shaking', 'shallow', 'shout', 'silly', 'simplest', 'slight', 'slip', 'slope', 'soap', 'solar', 'species', 'spin', 'stiff', 'swung', 'tales', 'thumb', 'tobacco', 'toy', 'trap', 'treated', 'tune', 'University', 'vapor', 'vessels', 'wealth', 'wolf', 'zoo']; 
 
 // Set number of attempts
 var lives = 8;
 var guesses = 0;
 var wrongGuesses = [];
 // Pick a random number
-var number = 0;
-//var number = Math.floor((Math.random() * words.length) + 1);
+//var number = 0;
+var number = Math.floor((Math.random() * words.length) + 1);
 // Pick word from number 
 var word = words[number].toLowerCase();
 // Count word length
@@ -25,9 +25,11 @@ var renderPlayArea = function(type, letter, letterPositions) {
       playArea[i] = ("_");
     // if correct guess, display guessed letter(s)
     } else if (type == "update") {
-      if (word[i] === letterPositions[i]) {
-        playArea[i] = letterPositions[i];
-      } else {
+      // if letterPositions exists
+      if (letterPositions) {
+        if (word[i] === letterPositions[i]) {
+          playArea[i] = letterPositions[i];
+        }
       }
     }
   }
@@ -63,28 +65,15 @@ var guess = function(letter) {
     renderPlayArea("update", letter, letterPositions);
   // if there are lives available 
   } else if ( guesses < lives - 1 ) {
-    console.log("wrongGuesses: " + wrongGuesses);
-    console.log("letter: " + letter);
-    // if there has been at least one wrong guess, check if repeat guess
-    if (wrongGuesses.length > 0) {
-      // loop through wrong guesses
-      for (var i = 0; i < wrongGuesses.length; i++) {
-        // if it is a repeat guess, tell user
-        if (wrongGuesses[i] == letter) {
-          console.log("You've already tried that letter");
-          renderPlayArea("update", letter, letterPositions);
-        // if it's not a repeat guess, dock a life
-        } else {
-          wrongGuesses.push(letter);
-          guesses += 1;
-          renderPlayArea("update", letter, letterPositions);
-        }
-      }
-    // if there have been no wrong guesses
-    } else {
+    if (guesses < 1) {
       wrongGuesses.push(letter);
       guesses += 1;
       renderPlayArea("start");
+      console.log("Letter not in word");
+    } else {
+      wrongGuesses.push(letter);
+      guesses += 1;
+      renderPlayArea("update", letter);
       console.log("Letter not in word");
     }
   // game over condition
