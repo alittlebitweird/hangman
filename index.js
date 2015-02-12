1b136c125f0e9af0949ff9d4233d0853de12a547
Nick Camillo <nick.camillo@gmail.com>
2015-02-12 13:31:59 -0500

    got hangman images loading properly. now to replace instead of append

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var swords = ['happy'];
 var words = ['acres', 'adult', 'advice', 'arrangement', 'attempt', 'August', 'Autumn', 'border', 'breeze', 'brick', 'calm', 'canal', 'Casey', 'cast', 'chose', 'claws', 'coach', 'constantly', 'contrast', 'cookies', 'customs', 'damage', 'Danny', 'deeply', 'depth', 'discussion', 'doll', 'donkey', 'Egypt', 'Ellen', 'essential', 'exchange', 'exist', 'explanation', 'facing', 'film', 'finest', 'fireplace', 'floating', 'folks', 'fort', 'garage', 'grabbed', 'grandmother', 'habit', 'happily', 'Harry', 'heading', 'hunter', 'Illinois', 'image', 'independent', 'instant', 'January', 'kids', 'label', 'Lee', 'lungs', 'manufacturing', 'Martin', 'mathematics', 'melted', 'memory', 'mill', 'mission', 'monkey', 'Mount', 'mysterious', 'neighborhood', 'Norway', 'nuts', 'occasionally', 'official', 'ourselves', 'palace', 'Pennsylvania', 'Philadelphia', 'plates', 'poetry', 'policeman', 'positive', 'possibly', 'practical', 'pride', 'promised', 'recall', 'relationship', 'remarkable', 'require', 'rhyme', 'rocky', 'rubbed', 'rush', 'sale', 'satellites', 'satisfied', 'scared', 'selection', 'shake', 'shaking', 'shallow', 'shout', 'silly', 'simplest', 'slight', 'slip', 'slope', 'soap', 'solar', 'species', 'spin', 'stiff', 'swung', 'tales', 'thumb', 'tobacco', 'toy', 'trap', 'treated', 'tune', 'University', 'vapor', 'vessels', 'wealth', 'wolf', 'zoo']; 
 
+var hangmanPics = [
+  "&nbsp;&nbsp;+---+\n &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|\n =========\n"
+];
+
+hangman1 = [ 
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|', 
+  '=========' 
+]; 
+
+
 // Set number of attempts
 var lives = 8;
 var guesses = 0;
@@ -39,7 +54,7 @@ var renderPlayArea = function(type, letter, letterPositions) {
   console.log(guessesRemaining); 
   if (type == "start") {
     // create GUI elements
-    $(".ide").html("<h2 class='guesses-remaining'></h2><h2 class='wrong-guesses'></h2><h2 class='instructions'></h2><h2 class='play-area'></h2><h2 class='win'></h2><form class='guess'><input type='text', placeholder='guess a letter', name: 'guess'/></form>");
+    $(".ide").html("<h2 class='guesses-remaining'></h2><h2 class='wrong-guesses'></h2><h2 class='instructions'></h2><h2 class='play-area'></h2><h2 class='win'></h2><h2 class='hangman-frame'></h2><form><input type='text', placeholder='guess a letter', name='guess'/></form>");
   }
   $(".guesses-remaining").html(guessesRemaining);
   
@@ -55,6 +70,8 @@ var renderPlayArea = function(type, letter, letterPositions) {
   console.log("Guess a letter with guess(letter)");
   
   // Play Area
+
+  renderHangman(wrongGuesses);
   console.log("\n\n");
   var playAreaString = playArea.toString().split(',').join(' ');
   $(".play-area").html(playAreaString);
@@ -69,13 +86,14 @@ var renderPlayArea = function(type, letter, letterPositions) {
 
 };
 // Guesses
-$(".guess").keypress(function(event) {
-  if (event.which == 13) {
-    event.preventDefault();
-    var $input = $('.guess :input');
-    $("form").submit();
-    guess($input);
-  }
+// get letter from form field
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+      e.preventDefault();
+      var value = $('input[name=guess]').val();
+      // pass letter to guess function
+      guess(value);
+    }
 });
 
 var guess = function(letter) {
@@ -101,6 +119,7 @@ var guess = function(letter) {
   // game over condition
   } else {
     console.log("Hangman. Game Over.");
+    $('.ide').append("<h2>Hangman, Game Over!</h2>");
   }
 };
 
@@ -109,5 +128,83 @@ $( document ).ready(function() {
   console.log(word);
   renderPlayArea("start");
 });
+// Build Hangman Frame
+var renderHangman = function(wrongGuesses) {
+  for (var h = 0; h < 7; h++) {
+    $('.hangman-frame').append(hangmanFrames[wrongGuesses.length][h] + "<br />");
+  }
+};
 
+
+hangman1 = [
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '========='
+];
+
+hangman2 = [
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;o&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '========='
+];
+
+hangman3 = [
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;o&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '========='
+];
+
+hangman4 = [
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;o&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;/|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '========='
+];
+
+hangman5 = [
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;o&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;/|\&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '========='
+];
+
+hangman6 = [
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;o&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;/|\&nbsp;&nbsp;|',
+  '&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '========='
+];
+
+hangman7 = [
+  '&nbsp;&nbsp;+---+',
+  '&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;o&nbsp;&nbsp;&nbsp;|',
+  '&nbsp;/|\&nbsp;&nbsp;|',
+  '&nbsp;/&nbsp;\&nbsp;&nbsp;|',
+  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|',
+  '========='
+];
+
+var hangmanFrames = [hangman1, hangman2, hangman3, hangman4, hangman5, hangman6, hangman7];
 
